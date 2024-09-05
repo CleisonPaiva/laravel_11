07f17faa0f55fbe11cee0135ea995fb5e42b1adc
Cleison Mendes <cleison606@gmail.com>
2024-09-05 17:27:54 -0300

    Delete resources/views/admin/users/partials/fields.blade.php

--- resources/views/admin/users/partials/fields.blade.php
+++
@@ -1,7 +0,0 @@
-<x-alert></x-alert>
-@csrf()
-<input name="name" type="text" placeholder="Nome" value="{{$user->name ?? old('name') }}">
-<input name="email" type="email" placeholder="E-mail" value="{{$user->email ?? old('email') }}">
-<input name="password" type="password" placeholder="Password">
-
-<button type="submit">Salvar</button>
